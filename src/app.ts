9b89d2cf619cad1a607d772437d32f5f40d7160d
ShiningElektra <elektrathanos@googlemail.com>
2021-03-02 14:36:28 +0100

    🚧 Add program structure

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,24 @@
-console.log("Welcome to PasswordDicer");
+import { hasAccess } from "./commands";
+import { printNoAccess, printWelcomeMessage } from "./messages";
+import { askForCredentials } from "./questions";
+
+const run = async () => {
+  printWelcomeMessage();
+  const credentials = await askForCredentials();
+  if (!hasAccess(credentials.masterPassword)) {
+    printNoAccess();
+    run();
+    return;
+  }
+
+  // const action = await askForAction();
+  // switch (action.command) {
+  //   case "set":
+  //     handleSetPassword(action.passwordName);
+  //     break;
+  //   case "get":
+  //     handelGetPassword(action.passwordName);
+  //     break;
+  // }
+};
+run();
